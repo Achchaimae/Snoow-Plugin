cb57f817ad082067b13d70ebdb58f094268a8106
achchaimae <achchaimae1@gmail.com>
2023-03-10 20:31:07 +0100

    little update in the index 👌🍀

--- wp-content/plugins/snoow-plugin/index.php
+++ wp-content/plugins/snoow-plugin/index.php
@@ -1,11 +1,2 @@
 <?php 
 //not sure if this is the right way to do it
-if ( ! defined( 'ABSPATH' ) ) {
-    exit;
-}
-defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
-
-if ( ! function_exists( 'add_action' ) ) {
-    echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
-    exit;
-}
